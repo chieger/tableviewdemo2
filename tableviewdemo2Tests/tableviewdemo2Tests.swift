cbaacb6bf19f0a7c88752b67ff02cc91fcfea285
chieger <charleshieger@CharlessMacBook.att.net>
2015-06-19 15:45:09 -0700

    Initial Commit

---
+++ tableviewdemo2Tests/tableviewdemo2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  tableviewdemo2Tests.swift
+//  tableviewdemo2Tests
+//
+//  Created by Charles Hieger on 6/19/15.
+//  Copyright (c) 2015 Charles Hieger. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class tableviewdemo2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
